2b3f02cb2bd2283885041e4b9c47009dd102fc26
7° Tecnica <7 Tecnica@192.168.0.51>
2018-05-23 11:20:35 -0300

    hola

--- Punto 4 tp investigacion/src/Texto.java
+++ Punto 4 tp investigacion/src/Texto.java
@@ -1,10 +1,7 @@
 
 public class Texto {
 
-	private String  texto = null;		
-	private String palabras [] = null;  
 	
-	public Texto(String txt) {
 		this.texto = txt;
 		this.palabras = txt.split(" ");
 	}
